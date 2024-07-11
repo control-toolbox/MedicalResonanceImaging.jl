f41735fffa998b51cff2c31e47128c0a7f2d6ab1
Documenter.jl <documenter@juliadocs.github.io>
2024-07-11 15:38:41 +0000

    build based on 41e4373

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#Optimal-control-in-Medical-Resonance-Imaging","page":"Introduction","title":"Optimal control in Medical Resonance Imaging","text":"","category":"section"}]
+}
